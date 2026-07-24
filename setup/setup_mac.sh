ad03a4fa5723990ef03d536798a80d6d0457f43a
innurhap28 <kmugi118@gmail.com>
2026-07-25 05:10:10 +0900

    init: 프로젝트 초기화

---
+++ setup/setup_mac.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "===== B5-1 SQL 환경 설치 ====="
+
+brew install sqlite
+brew install --cask dbeaver-community
+
+echo ""
+echo "===== 설치 완료 ====="
---
+++ setup/setup_win.ps1
@@ -0,0 +1,7 @@
+Write-Host "===== B5-1 SQL 환경 설치 ====="
+
+#winget install SQLite.SQLite
+winget install DBeaver.DBeaver.Community
+
+Write-Host ""
+Write-Host "===== 설치 완료 ====="
