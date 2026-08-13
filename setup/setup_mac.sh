b703b557609de022f272522aeb9573990cdfc252
innurhap28 <kmugi118@gmail.com>
2026-08-14 04:10:50 +0900

    feat: SQLite 설치 여부 확인 및 설치 스크립트 작성

--- setup/setup_mac.sh
+++ setup/setup_mac.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 
+if ! command -v brew > /dev/null 2>&1; then
+    echo "Homebrew가 설치되어 있지 않습니다."
+    exit 1
+fi
+    
 echo "===== B5-1 SQL 환경 설치 ====="
 
-brew install sqlite
-brew install --cask dbeaver-community
+# SQLite 설치 여부 확인 및 설치
+if command -v sqlite3 > /dev/null 2>&1; then
+    echo "SQLite already installed"
+else
+    echo "installing SQLite..."
+
+    if brew install sqlite; then
+        echo "SQlite install sucessed"
+    else
+        echo "failed to install SQLite"
+        exit 1
+    fi
+fi
+
+# DBeaver 설치 여부 확인 및 설치
+if brew list --cask dbeaver-community > /dev/null 2>&1; then
+    echo "DBeaver already installed"
+else
+    echo "installing DBeaver..."
+
+    if brew install --cask dbeaver-community; then
+        echo "DBeaver install sucessed"
+    else
+        echo "failed to install DBeaver"
+        exit 1
+    fi
+fi
 
 echo ""
 echo "===== 설치 완료 ====="
--- setup/setup_win.ps1
+++ setup/setup_win.ps1
@@ -1,7 +1,34 @@
 Write-Host "===== B5-1 SQL 환경 설치 ====="
 
-#winget install SQLite.SQLite
-winget install DBeaver.DBeaver.Community
+# SQLite
+if (winget list --id SQLite.SQLite) {
+    Write-Host "SQLite already installed"
+}
+else {
+    Write-Host "Installing SQLite..."
+    winget install --id SQLite.SQLite
+    if ($LASTEXITCODE -eq 0) {
+        Write-Host "SQLite installation completed"
+    }
+    else {
+        Write-Host "Failed to install SQLite"
+    }
+}
+
+# DBeaver
+if (winget list --id DBeaver.DBeaver.Community) {
+    Write-Host "DBeaver already installed"
+}
+else {
+    Write-Host "Installing DBeaver..."
+    winget install --id DBeaver.DBeaver.Community
+    if ($LASTEXITCODE -eq 0) {
+        Write-Host "DBeaver installation completed"
+    }
+    else {
+        Write-Host "Failed to install DBeaver"
+    }
+}
 
 Write-Host ""
 Write-Host "===== 설치 완료 ====="
